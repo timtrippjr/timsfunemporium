4b093bb352a99d7e4c130b2639f726081bd643e7
Tripp <ti.n.tripp@baypath.net>
2026-06-05 10:16:37 -0400

    canvas prints user joined, hides rooms list and shows canvas

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+const roomsCont = document.getElementById('roomsContainer');
+const gameCont = document.getElementById('gameContainer');
+gameCont.style.display = 'none';
+
+class Game{
+    constructor(gameId){
+        this.canvas = document.getElementById(gameId);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.ctx.fillStyle = 'white';
+        this.ctx.fillRect(
+            0, 0, 
+            this.canvas.clientWidth, 
+            this.canvas.clientHeight
+        );
+    }
+}
+
 socket.on('rooms updated', (rooms) => {
     console.log('refresh rooms',rooms);
 
@@ -7,28 +25,33 @@ socket.on('rooms updated', (rooms) => {
     roomsDiv.innerText = '';
 
     for (const [room, users] of Object.entries(rooms)){
-
-        if (room == socket.id) continue;
+        //if (room == socket.id) continue;
 
         const roomDiv = newElem('div', roomsDiv);
         roomDiv.className = 'room';
 
         const roomTitle = newElem('strong', roomDiv);
         roomTitle.innerText = room;
+        if (room == socket.id) {
+            roomTitle.innerText += ' (you)';
+        }else{
+            const kawaiiJoinButton = newElem('button', roomDiv);
+            kawaiiJoinButton.innerText = 'join button bruh bruh';
+            kawaiiJoinButton.style = 'float:right;'
+            kawaiiJoinButton.className = 'joinButton';
+            kawaiiJoinButton.addEventListener('click', (e)=>{
+                socket.emit('join game', room);
+            });
+        }
 
-        const kawaiiJoinButton = newElem('button', roomDiv);
-        kawaiiJoinButton.innerText = 'join button bruh bruh';
-        kawaiiJoinButton.style = 'float:right;'
-        kawaiiJoinButton.className = 'joinButton';
-        kawaiiJoinButton.addEventListener('click', (e)=>{
-            socket.emit('join game', room);
-        });
 
         console.log(users);
         const innerRooms = newElem('ul', roomDiv);
         for (const user of users){
             const innerRoom = newElem('li', innerRooms);
             innerRoom.innerText = user;
+            if (user == socket.id) 
+                innerRoom.innerText += ' (you)';
         }
     }
 });
@@ -39,17 +62,22 @@ socket.on('connect', ()=>{
 });
 
 // show game board, hide available rooms.
-socket.on('successfully joined', (data)=>{
-    console.log(socket.id + ' joined ' + data);
+socket.on('successfully joined', (room)=>{
+    console.log(socket.id + ' joined ' + room);
     /*const notif = newElem('div', document.getElementById('content'));
     notif.className += 'successnotif ';
-    notif.innerHTML = 'successfully joined user: ' + data;*/
+    notif.innerHTML = 'successfully joined user: ' + room;*/
 
-    //document.getElementById('roomsContainer').remove();
+    roomsCont.style.display = 'none';
+    gameCont.style.display = 'block';
+
+    const game = new Game('game');
+
+    game.ctx.font = "bold 30px Arial";      // Sets size and font family
+    game.ctx.fillStyle = "blue";            // Sets solid text color
+    game.ctx.strokeStyle = "black";         // Sets outline color
+    game.ctx.lineWidth = 2;                 // Sets outline thickness
+
+    // 3. Draw the text (text, x, y)
+    game.ctx.fillText('i joined '+room, 50, 100);    // Draws solid text
 });
-
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
